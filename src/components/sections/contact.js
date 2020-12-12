8e2684af9eeae45d74bfe57ba5a93128cb429196
Sajal Goyal <sajalg@iitk.ac.in>
2020-12-12 11:49:20 +0530

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -52,7 +52,7 @@ const Contact = ({ data }) => {
   return (
     <StyledContainer id="contact" ref={revealContainer}>
       <StyledHeading>What&apos;s Next?</StyledHeading>
-      <a href="https://info.flagcounter.com/4dnn"><img src="https://s01.flagcounter.com/count2/4dnn/bg_00FFC3/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
+      <a href="https://info.flagcounter.com/4dnn"><img src="https://s01.flagcounter.com/count2/4dnn/bg_00FFC3/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0" /></a>
       <StyledTitle>{title}</StyledTitle>
 
       <div dangerouslySetInnerHTML={{ __html: html }} />
